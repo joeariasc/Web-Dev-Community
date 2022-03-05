0f57d54d5cdbaa9f499f1d6921679839d3f6dc2d
Joe Arias <jarias@wawand.co>
2022-03-05 15:02:56 -0500

    adding connection file

---
+++ petstore/cmd/connection/connection.go
@@ -0,0 +1,70 @@
+package connection
+
+import (
+	"fmt"
+	"log"
+	"petstore/manage/client"
+	"petstore/manage/pet"
+	"petstore/manage/tools"
+
+	"github.com/gobuffalo/pop/v6"
+)
+
+var db *pop.Connection
+
+func init() {
+	pop.Debug = true
+	pop.AddLookupPaths("../../")
+
+	if err := pop.LoadConfigFile(); err != nil {
+		log.Panic(err)
+	}
+
+	var err error
+	db, err = pop.Connect("development")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println("Conected to database!")
+}
+
+func SavePet(Pet pet.Pet) {
+	if err := db.Create(&Pet); err != nil {
+		log.Fatal("error guardando la nueva mascota", err.Error())
+	}
+
+	pets := pet.Pets{}
+
+	if err := db.All(&pets); err != nil {
+		log.Fatal("error listando las mascotas", err.Error())
+	}
+
+	res, err := tools.PrettyStruct(pets)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("All Pets", res)
+}
+
+func SaveClient(Client client.Client) {
+	if err := db.Create(&Client); err != nil {
+		log.Fatal("error guardando el client", err.Error())
+	}
+
+	clients := client.Clients{}
+
+	if err := db.All(&clients); err != nil {
+		log.Fatal("error listando los clientes", err.Error())
+	}
+
+	res, err := tools.PrettyStruct(clients)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("All Clients", res)
+}
